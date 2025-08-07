c2133b560327785e476f70fcd55bc630df79ac3c
Kimberley Chan <cmaylynn12@gmail.com>
2025-08-07 10:52:05 +0200

    chore: setup according to documentation
    Set up initial project wrappers according to Chakra and Apollo Client
    documentation
    Set up basic imports for use
    Removed styling in page and globals css to avoid conflict with Chakra UI
    elements

---
+++ src/contexts/AppWrapper.tsx
@@ -0,0 +1,20 @@
+import { Provider } from "@/components/ui/provider";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ReactNode } from "react";
+
+type AppWrapperProps = {
+  children: ReactNode;
+};
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+export const AppWrapper = ({ children }: AppWrapperProps) => {
+  return (
+    <ApolloProvider client={client}>
+      <Provider>{children}</Provider>
+    </ApolloProvider>
+  );
+};

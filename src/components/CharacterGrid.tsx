34b927c24ad56121c1c4307d280280d8fe999007
Kimberley Chan <cmaylynn12@gmail.com>
2025-08-08 20:27:29 +0200

    fix(character grid): add transitions to reduce flashing
    Added transition between page changes to avoid flashing.
    Smoother transition.

--- src/components/CharacterGrid.tsx
+++ src/components/CharacterGrid.tsx
@@ -1,24 +1,30 @@
 import AuthContext from "@/contexts/AuthWrapper";
 import { Character } from "@/types/Character";
 import { Flex, Center, SimpleGrid } from "@chakra-ui/react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CharacterCard from "./CharacterCard";
 import { GET_CHARACTERS_QUERY } from "@/queries/getCharacters";
 import { useQuery } from "@apollo/client";
+import { keyframes } from "@emotion/react";
 
 type CharacterGridProps = {
   page: number;
 };
 
 export const CharacterGrid = ({ page }: CharacterGridProps) => {
+  const [animateKey, setAnimateKey] = useState(page);
   const { username, jobTitle } = useContext(AuthContext);
   const authenticated = username && jobTitle;
 
-  const { error, data } = useQuery(GET_CHARACTERS_QUERY, {
+  const { loading, error, data } = useQuery(GET_CHARACTERS_QUERY, {
     variables: { page },
     fetchPolicy: "cache-and-network",
     skip: !authenticated, // Only run query if user is authenticated
   });
+
+  useEffect(() => {
+    setAnimateKey(page);
+  }, [page]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -29,22 +35,37 @@ export const CharacterGrid = ({ page }: CharacterGridProps) => {
     return;
   }
 
+  const fadeIn = keyframes`
+  from {opacity: 0;}
+  to {opacity: 1}`;
+
   return (
-    <Flex className="info-page" direction="column" minH="400px">
+    <Flex
+      overflow="hidden"
+      className="info-page"
+      direction="column"
+      minH="695px"
+      key={animateKey}
+      animation={`${fadeIn} 0.7s ease`}
+    >
       <main>
         {/* Character Grid */}
-        <Center marginBottom="20px" transition="backgrounds 0.2s ease-in-out">
-          <SimpleGrid columns={5} gap="25px">
-            {data?.characters.results.map(
-              (character: Character, index: number) => (
-                <CharacterCard
-                  character={character}
-                  key={`character-${index}`}
-                />
-              )
-            )}
-          </SimpleGrid>
-        </Center>
+        {!loading ? (
+          <Center marginBottom="20px">
+            <SimpleGrid columns={5} gap="25px">
+              {data?.characters.results.map(
+                (character: Character, index: number) => (
+                  <CharacterCard
+                    character={character}
+                    key={`character-${index}`}
+                  />
+                )
+              )}
+            </SimpleGrid>
+          </Center>
+        ) : (
+          <Center height="100%" width="100%"></Center>
+        )}
       </main>
     </Flex>
   );
